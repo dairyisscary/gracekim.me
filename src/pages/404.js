7a1bdd60c217416faff7abe33cb33781b1665cd9
Eric Butler <idealeric@gmail.com>
2018-01-30 21:04:49 -0500

    Add a 404 Page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Page from "common/page";
+
+function FourOhFour() {
+  return (
+    <Page title="Page Not Found">
+      <div>
+        <p>Unable to find this page (that&apos;s a 404!). Are you sure this is a valid link?</p>
+      </div>
+    </Page>
+  );
+}
+
+export default FourOhFour;
