2b691862d694fa7a57bd0bbf49d71b8d4f62ec08
Eric Butler <eric.butler@notarize.com>
2020-05-16 13:42:46 -0700

    Upgrade Fathom Embed

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-exports.onRouteUpdate = ({ location }) => {
-  if (
-    process.env.NODE_ENV === "production" &&
-    typeof window.fathom === "function" &&
-    location.hostname === "gracekim.me"
-  ) {
-    window.fathom("trackPageview");
-  }
-};
